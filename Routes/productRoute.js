c9199c9b6c99240a794911b59da4a9f21d4da80f
vikram-42-alla <allatrivikram@gmail.com>
2025-06-03 15:58:00 +0530

    commit

--- Routes/productRoute.js
+++ Routes/productRoute.js
@@ -30,6 +30,20 @@ app.get("/get",async(req,res)=>{
         
     }
 })
+app.put("/update",async(req,res)=>{
+  try {
+    const { id, title, price, offerPrice, discountPercentage, description, category, image, reviews } = req.body;
+    const updateProduct=await Product.findOneAndUpdate(
+      {id},
+      {title, price, offerPrice, discountPercentage, description, category, image, reviews},
+      {new:true}
+    )
+    if(!updateProduct)return res.status(404).json({message:"Product not found"})
+    res.status(200).json({message:"Product Updated"})
+  } catch (error) {
+    console.log(error)
+  }
+})
 app.delete("/remove", async (req, res) => {
   try {
     const { id } = req.body;
